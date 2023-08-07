9a5833c05677f989539d0efd698980c13809f8ec
mlprtr87 <hlangari1353@gmail.com>
2023-08-07 15:00:20 +0330

    printf and bugs fixed

--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -3,11 +3,10 @@
 #include "stdio.h"
 
 void main() {
-    int x = 10;
     clearScr();
     printf("Welcome to IDK OS!\n");
     printf("stage2 Loaded Successfully!\n");
     printf("Switching to 32 bit Pmode...\tDone!\n");
-    printf("Switching language to C%p...\tDone!\n", &x);
+    printf("Switching language to C...\tDone!\n");
     while(1) {}
 }
